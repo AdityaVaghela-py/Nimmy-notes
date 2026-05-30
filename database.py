81610ca3974b0dc0c758d0ebfae40744b9766759
Aditya Vaghela <vaghela.aditya@outlook.com>
2026-05-30 14:34:41 +0530

    Add add_nimmy function and move table creation to create_tables()

--- database.py
+++ database.py
@@ -24,15 +24,10 @@ def get_db():
     with pool.connection() as conn:
         yield conn
 
-TABLE_NIMMIES = "nimmies"
-TABLE_TAGS = 'tags'
-TABLE_NIMMYTAGS = "nimmytags"
-
-with pool:
-    with pool.connection() as conn:
-        with conn.cursor() as c:
-            c.execute(f"""CREATE TABLE IF NOT EXISTS {TABLE_NIMMIES}(
-                  id SERIAL PRIMARY KEY,
+def create_tables(conn: psycopg.Connection):
+    with conn.cursor() as c:
+        c.execute("""CREATE TABLE IF NOT EXISTS nimmies(
+                  id INTEGER GENERATED ALWAYS AS IDENTITY PRIMARY KEY,
                   title VARCHAR(128) NOT NULL,
                   content VARCHAR(1024) NOT NULL,
                   date_created TIMESTAMPTZ DEFAULT CURRENT_TIMESTAMP,
@@ -41,9 +36,34 @@ with pool:
                   is_pinned BOOLEAN DEFAULT FALSE,
                   is_deleted BOOLEAN DEFAULT FALSE)""")
 
-            c.execute(f"""CREATE TABLE IF NOT EXISTS {TABLE_TAGS}(
-                  id SERIAL PRIMARY KEY,
+        c.execute("""CREATE TABLE IF NOT EXISTS tags(
+                  id INTEGER GENERATED ALWAYS AS IDENTITY PRIMARY KEY,
                   name VARCHAR(64) UNIQUE)""")
             
-            c.execute(f"CREATE TABLE IF NOT EXISTS {TABLE_NIMMYTAGS}(nimmy_id INTEGER, tag_id INTEGER, UNIQUE(nimmy_id, tag_id))")
+        c.execute("CREATE TABLE IF NOT EXISTS nimmytags (nimmy_id INTEGER, tag_id INTEGER, UNIQUE(nimmy_id, tag_id))")
 
+def add_nimmy(nimmy_title: str, nimmy_content: str, nimmy_tags: list, conn: psycopg.Connection):
+
+    with conn.cursor() as c:
+
+        c.execute('INSERT INTO nimmies (title, content) VALUES (%s, %s) RETURNING id AS nimmy_id', (nimmy_title, nimmy_content))
+        nimmy_id = c.fetchone()['nimmy_id']
+
+        if not nimmy_tags:
+            return
+        
+        for tag in nimmy_tags:
+
+            c.execute('SELECT id from tags WHERE name = %s', (tag,))
+            row = c.fetchone()
+
+            if row is not None:
+
+                c.execute('INSERT into nimmytags (nimmy_id, tag_id) VALUES (%s, %s)', (nimmy_id, row['id']))
+
+                continue
+                
+            c.execute('INSERT INTO tags (name) VALUES (%s) RETURNING id AS tag_id', (tag,))
+            tag_id = c.fetchone()['tag_id']
+
+            c.execute('INSERT into nimmytags (nimmy_id, tag_id) VALUES (%s, %s)', (nimmy_id, tag_id))
