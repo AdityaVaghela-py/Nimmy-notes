c52b0bfa77c0faebf227445e0fd4398f6ed72eb1
Aditya Vaghela <vaghela.aditya@outlook.com>
2026-06-01 11:44:18 +0530

    initiate FastAPI application, add note creation endpoint and database lifecycle management

---
+++ main.py
@@ -0,0 +1,33 @@
+from fastapi import FastAPI, Depends
+from contextlib import asynccontextmanager
+from pydantic import BaseModel, Field
+from typing import List
+
+from psycopg import Connection
+import database as db
+
+class Nimmy(BaseModel):
+    title: str = Field(..., title="The name of the Nimmy", description="The title is used to identify a specific nimmy before getting into its details, ideal length is 128 or lesser", examples=["Do the math work in the morning after waking up", "Grocerry shopping", "Have to farm for flins in genshin impact"], max_length=128)
+
+    content: str = Field(..., title="Give your Nimmy more details", description="The description can further be used to give the Nimmy more information, in 1024 or lesser characters", examples=["I have scored 80+ in maths, but now i want to go higher, I am targeting for 95 this time", "Mom has asked me to buy some things from the market, andI too have noticed lack of grocery in the home, so I better consider it before we start lacking", "Genshin just released it's brand new character Flins and I am damn impressed with its skills, I WANT IT NOWWWWWW"], max_length=1024)
+
+    tags: List[str] = Field(default_factory=list, title="Tags make it easy to identify, search and sort Nimmies", description="Provide as many tags as you want in a list, or ommit this field if no tags are required for this Nimmy", examples=[['important', 'math'], ['important'], ['gaming', 'genshin', 'pulls']])
+
+
+@asynccontextmanager
+async def lifespan(app: FastAPI):
+    db.pool.open()
+
+    with db.pool.connection() as conn:
+        db.create_tables(conn)
+
+    yield
+
+    db.pool.close()
+
+
+app = FastAPI(lifespan=lifespan)
+
+@app.post('/nimmies')
+def add_nimmy(nimmy: Nimmy, conn: Connection = Depends(db.get_db)):
+    db.add_nimmy(nimmy.title, nimmy.content, nimmy.tags, conn)
